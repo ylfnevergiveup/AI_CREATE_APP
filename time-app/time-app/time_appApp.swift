231fedc3d0ab9d9eef693cb710a614e9ecba9cac
ylfnevergiveup <114249289+ylfnevergiveup@users.noreply.github.com>
2024-08-24 11:11:50 +0800

    Add files via upload

---
+++ time-app/time-app/time_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_appApp.swift
+//  time-app
+//
+//  Created by 叶李烽 on 2024/8/23.
+//
+
+import SwiftUI
+
+@main
+struct time_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
